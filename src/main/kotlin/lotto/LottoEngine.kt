86a912a45bb495a8afc6f50af33af288c3b414d9
HongJiBeom <hongjibeom@hongjibeom-ui-MacBookPro.local>
2025-04-28 07:32:09 +0900

    feat: Implements validation in LottoEngine

--- src/main/kotlin/lotto/LottoEngine.kt
+++ src/main/kotlin/lotto/LottoEngine.kt
@@ -4,7 +4,19 @@ class LottoEngine(
     private val winningNumbers: List<Int>,
     private val bonusNumber: Int,
     private val purchasedAmount: Int,
-    private val statisticResults: Map<WinningStatistic, Int>
+    private val statisticResults: Map<WinningStatistic, Int> = mapOf()
 ) {
-    
+    init {
+        validateWinningNumbers(winningNumbers)
+        require(bonusNumber in 1..45) { TODO() }
+        require(purchasedAmount >= 1000) { TODO() }
+    }
+
+    private fun validateWinningNumbers(winningNumbers: List<Int>) {
+        require(winningNumbers.size == 6) { "[ERROR] Lotto must contains exactly 6 numbers." }
+        winningNumbers.forEach {
+            require(it in 1..45) { TODO() }
+        }
+    }
+
 }
